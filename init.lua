eefc972a2708008412e24d11190ed2414922ac8b
Ryan White <ryan.white@expel.com>
2025-06-23 09:19:05 -0400

    Add shell ft

--- init.lua
+++ init.lua
@@ -945,5 +945,13 @@ set shiftwidth=2 smarttab
 ]],
 })
 
+-- Set shell/bash config
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = 'sh',
+  command = [[
+set shiftwidth=4 smarttab
+]],
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
